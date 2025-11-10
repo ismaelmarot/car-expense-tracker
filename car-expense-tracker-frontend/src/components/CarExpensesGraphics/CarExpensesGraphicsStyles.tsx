bf314021eb8f325257707567935f948a3b391538
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-10 20:27:33 -0300

    change: styles

--- car-expense-tracker-frontend/src/components/CarExpensesGraphics/CarExpensesGraphicsStyles.tsx
+++ car-expense-tracker-frontend/src/components/CarExpensesGraphics/CarExpensesGraphicsStyles.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import styled from '@emotion/styled';
 import {Grid, Typography } from '@mui/material';
 import { flex } from '../../helpers/setFlex';
+import { GeneralColors } from '../../constants/GeneralColors';
 
 export const Container = styled(Grid)`
-  ${flex('column','center','center')}
+  ${flex('row','center','center')}
   margin-bottom: 3rem;
 `;
 
 export const ContainerGraphics = styled(Grid)`
   width: 100%;
   border-radius: 3px;
-  border: 3px solid rgba(228,232,236,1);
+  border: 3px solid ${GeneralColors.grey};
 `;
 
 export const TitleStyled = styled(Typography)`
   width: 100%;
   margin-bottom: 1rem;
   padding: .3rem;
+  border-radius: 3px;
   font-size: 1.5rem;
   font-weight: bold;
-  border-radius: 3px;
-  color: rgba(36,99,235,1);
-  background-color: rgba(166,201,238,1);
+  color: ${GeneralColors.white};
+  background-color: ${GeneralColors.black};
 `;
