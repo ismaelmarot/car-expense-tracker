44f84282d4ffef426682d41d73b8863ef6d29969
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-11 18:30:53 -0300

    restyling

--- car-expense-tracker-frontend/src/constants/GeneralColors.tsx
+++ car-expense-tracker-frontend/src/constants/GeneralColors.tsx
@@ -1,7 +1,8 @@
 export const GeneralColors = {
     white: 'rgba(255, 255, 255, 1)',
     black: 'rgba(0, 0, 0, 1)',
-    grey: 'rgba(228,232,237,1)',
-    red: 'rgba(211, 47, 47 , 1)',
-    AppData: 'rgba(0,0,0,1)'
-};
+    grey: 'rgba(228, 232, 237, 1)',
+    red: 'rgba(211, 47, 47, 1)',
+    blue: 'rgba(24, 118, 208, 1)',
+    AppData: 'rgba(29, 31, 46, 1)'
+}; 
