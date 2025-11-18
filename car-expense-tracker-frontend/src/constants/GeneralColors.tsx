2b1e9b04eb89200e8178f1dc33d99fff762f2a5c
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-18 09:47:03 -0300

    restye: CarsPageStyles.tsx

--- car-expense-tracker-frontend/src/constants/GeneralColors.tsx
+++ car-expense-tracker-frontend/src/constants/GeneralColors.tsx
@@ -1,6 +1,7 @@
-export const barChartColors = {
+export const GeneralColors = {
     black: 'rgba(0, 0, 0, 1)',
     white: 'rgba(255, 255, 255, 1)',
     blue: 'rgba(36, 99, 235, 1)',
     red: 'rgba(234, 32, 39, 1)',
+    grey: 'rgb(221, 221, 221, 1)'
 };
--- car-expense-tracker-frontend/src/pages/CarPages/CarsPageStyles.tsx
+++ car-expense-tracker-frontend/src/pages/CarPages/CarsPageStyles.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 import { Grid, Typography } from '@mui/material';
+import { GeneralColors } from '../../constants/GeneralColors';
+import { flex } from '../../helpers/setFlex';
+import { size } from '../../helpers/setSize';
 
 export const GeneralContainer = styled(Grid)`
     margin-bottom: 3rem;
@@ -14,28 +17,23 @@ export const Container = styled(Grid)`
 export const Title = styled(Grid)`
     margin: 0 .5rem;
     border-radius: 3px;
-    background-color: rgba(166,201,238,1);
+    background-color: ${GeneralColors.blue};
 `;
 
 export const TypographyStyled = styled(Typography)`
     font-size: 2rem;
     font-weight: bold;
-    color: rgba(38,100,235,1);
+    color: ${GeneralColors.white};
 `;
 
 export const LinkCard = styled(Link)`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
+    ${flex('column', 'center', 'center')}
+    ${size('100%', '100%')}
     text-decoration: none;
     transition: font-size 0.3s ease;
     &:hover {
         font-size: 2rem;
-        border-color: rgba(166,201,238,1);
-        color: rgba(37,100,235,1);
+        color: ${GeneralColors.blue};
     }
 `;
 
@@ -44,13 +42,12 @@ export const LinkAddCarCard = styled(LinkCard)`
 `;
 
 export const CardStyled = styled(Grid)`
-    width: 100%;
-    height: 300px;
-    background-color: #f5f5f5;
-    border: 3px solid #ddd;
+    ${size('100%', '300px')}
+    background-color: ${GeneralColors.white};
+    border: 3px solid ${GeneralColors.grey};
     border-radius: 5px;
     transition: font-size 0.3s ease;
     &:hover {
-        border-color: rgba(166,201,238,1);
+        border-color: ${GeneralColors.blue};
     }
 `;
