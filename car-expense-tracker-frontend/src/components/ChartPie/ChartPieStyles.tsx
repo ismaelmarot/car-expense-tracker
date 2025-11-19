a4cc7ca2b2b0e05b21a0c787f4b2d77236cb6c7c
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-19 10:17:09 -0300

    restyling: DeletCarConfirm.tsx

--- car-expense-tracker-frontend/src/components/ChartPie/ChartPieStyles.tsx
+++ car-expense-tracker-frontend/src/components/ChartPie/ChartPieStyles.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Grid, Typography } from '@mui/material';
+import { GeneralColors } from '../../constants/GeneralColors';
+import { flex } from '../../mixins/setFlex';
+import { size } from '../../mixins/setSize';
 
 export const Container = styled(Grid)`
     margin-top: .5rem;
     padding-top: 1rem;
-    background-color: white;
+    background-color: ${GeneralColors.white};
 `;
 
 export const GridPieChart = styled(Grid)`
-    display: flex;
-    justify-content: center;
-    height: 22rem;
-    width: 90%;
+    ${flex('column','center','center')}
+    ${size('90%','22rem')}
     margin: 0 1rem 2rem;
-    background-color: white;
+    background-color: ${GeneralColors.white};
 `;
 
 export const TypographyStyled = styled(Typography)`
--- car-expense-tracker-frontend/src/components/DeletCarConfirmationDialog/DeletCarConfirmationDialogStyles.tsx
+++ car-expense-tracker-frontend/src/components/DeletCarConfirmationDialog/DeletCarConfirmationDialogStyles.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Button } from '@mui/material';
+import { GeneralColors } from '../../constants/GeneralColors'; 
 
 export const ButtonCancel = styled(Button)`
-    color: rgba(24, 118, 208, 1);
+    color: ${GeneralColors.blue};
 `;
 
 export const ButtonConfirm = styled(Button)`
     padding: .5rem;
-    color: rgba(255, 255, 255, 1);
-    background-color: rgba(211, 47, 47, 1);
+    color: ${GeneralColors.white};
+    background-color: ${GeneralColors.red};
 `;
