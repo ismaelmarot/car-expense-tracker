d6867062ba8a04e6605a7d4770ebba6a792e3589
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-19 08:15:54 -0300

    restyling: CarDetailsStyles.tsx

--- car-expense-tracker-frontend/src/components/CarDetails/CarDetailsStyles.tsx
+++ car-expense-tracker-frontend/src/components/CarDetails/CarDetailsStyles.tsx
@@ -2,27 +2,25 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { Grid, Box, Button } from '@mui/material';
 import { flex } from '../../mixins/setFlex';
+import { GeneralColors } from '../../constants/GeneralColors';
 
 export const BoxStyled = styled(Box)`
     ${flex('column', 'center', 'center')}
     height: 100vh;
 `;
 
-export const GridCarDetails = styled(Grid)`}
-    display: flex;
-    justify-content: senter;
+export const GridCarDetails = styled(Grid)`
+    ${flex('row', 'center', 'center')}
     margin: 0 .5rem;
-    background-color:rgba(228,232,237,1);
+    background-color: ${GeneralColors.grey};
 `;
 
 export const GridDeleteCarIcon = styled(Grid)`
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
+    ${flex('row', 'center', 'flex-end')}
     width: 100%;
     padding: 0 .5rem 0 1rem;
 `;
 
 export const ButtonDeleteCar = styled(Button)`
-    background-color: rgba(211, 47, 47 , 1);
+    background-color: ${GeneralColors.red};
 `;
